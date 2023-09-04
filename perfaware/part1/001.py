dd95acdcc785849ce98bb16b0b32c010d75617da
Brandon Makin <BrandonMakin@users.noreply.github.com>
2023-09-04 12:55:51 -0700

    listing 38 decompiles correctly
    fixed mistake in `registers` table.

--- perfaware/part1/001.py
+++ perfaware/part1/001.py
@@ -1,46 +1,45 @@
-# Emulate MOV
+# emulate mov
 
-"""
-table to decode W and R/M fields into their corresponding registers
+'''
+table to decode w and r/m fields into their corresponding registers
 usage:
 registers[w][rm] == the correct register
-"""
+'''
 registers = [
-    ["AL", "CL", "DL", "BL", "SP", "BP", "SI", "DI"],
-    ["AX", "CX", "DX", "BX", "AH", "CH", "DH", "BH"]
+    ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"],
+    ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"]
     ]
 
+with open("listing_0038_many_register_mov", "rb") as file:
+# with open("listing_0037_single_register_mov", "rb") as file:
+    print("bits 16\n")
 
-with open("listing_0037_single_register_mov", "rb") as file:
     while (bytes := list(file.read(2))):
         
         # first bit
         opcode = bytes[0] >> 2
 
-        """
+        '''
         if d == 0: reg field specifies instruction source (direction is from register)
         if d == 1: reg field specifies instruction destination (direction is to register)
-        """
+        '''
         d = bytes[0] >> 1 & 1
         
         w = bytes[0] >> 0 & 1
 
         # second bit
 
-        ## mod tells us whether this is a memory operation or a register operation
+        'mod tells us whether this is a memory operation or a register operation'
         mod = bytes[1] >> 6 # top 2 bytes
 
-        ##
         reg = bytes[1] >> 3 & 0b111 # next 3 bytes
         
-        ##
         rm  = bytes[1] >> 0 & 0b111 # last 3 bytes
 
-        print()
         match opcode:
-            case 0b100010: # MOV
+            case 0b100010: # mov
                 match mod:
-                    case 0b11: # Register Mode
+                    case 0b11: # register mode
                         source = registers[w][reg]  # set source to register specified by reg
                         destination = registers[w][rm] # set destination to register specified by rm
                         if (d): # if d: swap source and destination
