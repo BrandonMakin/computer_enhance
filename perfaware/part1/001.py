dab0c064a0f245f89151b0a6a2c65a86415c1c7e
Brandon Makin <BrandonMakin@users.noreply.github.com>
2023-09-05 00:45:46 -0700

    listing 39 decompiles correctly

--- perfaware/part1/001.py
+++ perfaware/part1/001.py
@@ -10,45 +10,92 @@ registers = [
     ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"]
     ]
 
-with open("listing_0038_many_register_mov", "rb") as file:
-# with open("listing_0037_single_register_mov", "rb") as file:
+with open("listing_0039_more_movs", "rb") as file:
+    def next(): return int.from_bytes(file.read(1))
+
     print("bits 16\n")
 
-    while (bytes := list(file.read(2))):
-        
-        # first bit
-        opcode = bytes[0] >> 2
+    while (byte := next()):
 
-        '''
-        if d == 0: reg field specifies instruction source (direction is from register)
-        if d == 1: reg field specifies instruction destination (direction is to register)
-        '''
-        d = bytes[0] >> 1 & 1
-        
-        w = bytes[0] >> 0 & 1
+        # MOV (register/memory to/from register)
+        if 0b100010 == byte >> 2:
+            '''
+            if d == 0: reg field specifies instruction source (direction is from register)
+            if d == 1: reg field specifies instruction destination (direction is to register)
+            '''
+            d = byte >> 1 & 1
 
-        # second bit
+            '''
+            if w == 0: move a byte
+            if w == 1: to move a word (2 bytes)
+            '''
+            w = byte >> 0 & 1
 
-        'mod tells us whether this is a memory operation or a register operation'
-        mod = bytes[1] >> 6 # top 2 bytes
+            # second bit
+            byte_2 = next()
+            
+            mod = byte_2 >> 6 # MOD (mode) tells us whether this is a memory operation or a register operation
+            reg = byte_2 >> 3 & 0b111 # REG (register)
+            rm  = byte_2 >> 0 & 0b111 # R/M (register/memory)
 
-        reg = bytes[1] >> 3 & 0b111 # next 3 bytes
-        
-        rm  = bytes[1] >> 0 & 0b111 # last 3 bytes
+            source = registers[w][reg]  # set source to register specified by reg
 
-        match opcode:
-            case 0b100010: # mov
-                match mod:
-                    case 0b11: # register mode
-                        source = registers[w][reg]  # set source to register specified by reg
-                        destination = registers[w][rm] # set destination to register specified by rm
-                        if (d): # if d: swap source and destination
-                            source, destination = destination, source
-                            print("hi")
+            if mod == 0b11: # register mode
+                destination = registers[w][rm] # set destination to register specified by rm
+                if (d): # if d: swap source and destination
+                    source, destination = destination, source                
+                print(f"mov {destination}, {source}")
+            else: # memory mode
+                address = ""
+                if mod == 0b00 and rm == 0b110: # direct address
+                    print ("mov with direct address, but I don't remember how to handle this!!")
+                else:
+                    # effective address calculation
+                    match rm:
+                        case 0:
+                            address = "bx + si"
+                        case 1:
+                            address = "bx + di"
+                        case 2:
+                            address = "bp + si"
+                        case 3:
+                            address = "bp + di"
+                        case 4:
+                            address = "si"
+                        case 5:
+                            address = "di"
+                        case 6:
+                            if mod == 0b00:
+                                address = "???"
+                            else: address = "bp"
+                        case 7:
+                            address = "bx"
+                    
+                    if mod != 0b00:
+                        if mod == 0b01:
+                            offset = next()
+                        if mod == 0b10:
+                            offset = next() + (next() * 0x100)
                         
-                        print(f"mov {destination}, {source}")
+                        if offset != 0:
+                            address += f" + {offset}"
                 
-                    case _:
-                        print ("unsupported memory mode")
-            case _:
-                print("unknown instruction")
+                destination = f"[{address}]"
+
+                if (d): # if d: swap source and destination
+                    source, destination = destination, source
+
+                print(f"mov {destination}, {source}")
+
+        # MOV (immediate to register)
+        elif 0b1011 == byte >> 4:
+            w = byte >> 3 & 1
+            reg = byte >> 0 & 0b111
+            immediate_value = next() # least significant byte
+            if w:
+                immediate_value += next() * 0x100 # most significant byte (only used for 2-byte registers)
+            print(f"mov {registers[w][reg]}, {immediate_value}")
+
+        else:
+            print("unexpected data (unknown instruction, or an instruction was longer than expected); exiting")
+            exit(1)
