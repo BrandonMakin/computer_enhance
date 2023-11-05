201169ae60abccef3adf2639a5fa7ad792f37b02
Brandon Makin <BrandonMakin@users.noreply.github.com>
2023-11-04 17:01:25 -0700

    MOVs w/ explicit sizes and direct address work

--- perfaware/part1/001.py
+++ perfaware/part1/001.py
@@ -9,9 +9,11 @@ registers = [
     ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"],
     ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"]
     ]
+rm_effective_address_calculation = ["bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"]
 
-with open("listing_0039_more_movs", "rb") as file:
+with open("listing_0040_challenge_movs", "rb") as file:
     def next(): return int.from_bytes(file.read(1))
+    def next_16(): return int.from_bytes(file.read(2), 'little')
 
     print("bits 16\n")
 
@@ -45,7 +47,7 @@ with open("listing_0039_more_movs", "rb") as file:
             else: # memory mode
                 address = ""
                 if mod == 0b00 and rm == 0b110: # direct address
-                    print ("mov with direct address, but I don't remember how to handle this!!")
+                    address = str(next_16())
                 else:
                     # effective address calculation
                     match rm:
@@ -70,8 +72,8 @@ with open("listing_0039_more_movs", "rb") as file:
                         if mod == 0b01:
                             offset = next()
                         if mod == 0b10:
-                            offset = next() + (next() * 0x100)
-                        
+                            offset = next_16()
+                            
                         if offset != 0:
                             address += f" + {offset}"
                 
@@ -90,6 +92,41 @@ with open("listing_0039_more_movs", "rb") as file:
             if w:
                 immediate_value += next() * 0x100 # most significant byte (only used for 2-byte registers)
             print(f"mov {registers[w][reg]}, {immediate_value}")
+
+        elif 0b1100011 == byte >> 1: # MOV (immediate to register/memory)
+            w = byte & 1
+            byte_2 = next()
+            mod = byte_2 >> 6
+            rm = byte_2 & 0b111
+
+            address = rm_effective_address_calculation[rm]
+            offset = 0 # won't work if there is a defined offset equal to 0, but can that ever happen?
+
+            match mod:
+                case 0b00:
+                    if rm == 0b110: # direct address. 16-bit displacement to follow.
+                        address = str(next_16())
+                case 0b01:
+                    offset = next()
+                case 0b10:
+                    offset = next_16()
+                case 0b11:
+                    print("If this text appears, IDK why MOV (immediate to register/memory) is being called in register mode, but apparently it is.")
+                    print("Or I'm really misunderstanding something.")
+                    print("Because it seems like the assembly should just assemble to MOV (immediate to register) instead.")
+                    print("Let's bail before things break any more.")
+                    exit(1)
+            
+            immediate_value = ""
+            if w:
+                immediate_value = f"word {next_16()}"
+            else:
+                immediate_value = f"byte {next()}"
+
+            if (offset != 0):
+                address = address + f" + {offset}"
+            
+            print(f"mov [{address}], {immediate_value}")
 
         else:
             print("unexpected data (unknown instruction, or an instruction was longer than expected); exiting")
